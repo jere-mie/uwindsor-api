a61a1514f52afd5d5e59a93b90b0ef57c17f5863
jere-mie <jere-mie@users.noreply.github.com>
2021-11-20 00:39:54 -0500

    added webpage to api

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # UWindsor API
 An API for UWindsor data (data not included here)
 
-#### Note: For this api to work, there must be a 'static' folder in the same directory, containing all of the necessary .json files listed in the `keys` dictionary in `app.py`
+#### Note: For this API to work, there must be a 'datasets' folder in the 'static' folder, containing all of the necessary sub-folders and json files listed in the `keys` dictionary in `app.py`.
+
+
+### API Endpoints
+| Dataset      | API Endpoint | Parameter | Description |
+| :-----------: | : -----------: | : -----------: | : -----------: |
+| Courses | /api/courses | Course code | Contains course code, title, and description |
+| Staff | /api/staff | Staff name | Contains name, email, phone, title, department, and location |
+| Buildings | /api/buildings | Building name | Contains building name and location (latitude and longitude) |
+| Holidays | /api/holidays | Date (in format YYYY-MM-DD) | Contains date and what the holiday/date is |
+| Emergency Services | /api/emergency_services | Service name (ex. Campus Police) | Contains phone, extension, service name, and website |
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, render_template, url_for, flash, redirect
 import json
 from difflib import SequenceMatcher
 from flask_cors import CORS, cross_origin
+import sys
+import os
 
 def similar(a,b): # returns how similar two strings are
     return SequenceMatcher(None, a, b).ratio()
@@ -15,6 +17,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = data['secret_key']
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+# home webpage
+@app.route('/home')
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# guide page
+@app.route('/guide')
+def guide():
+    return render_template('guide.html')
 
 @app.route('/api/<choice>', methods=['GET'])
 @cross_origin()
@@ -30,25 +43,33 @@ def api(choice):
     if choice not in keys: # if the route is invalid, for example /api/rooch
         return make_response(jsonify(f'/api/{choice} is not a valid route'), 400)
 
-    data = "" # getting json from file
-    with open(f'static/{choice}.json', 'r') as f:
-        data = json.loads(f.read())
+    jsondata = "" # getting json from file
+    with open(f'static/datasets/{choice}/{choice}.json', 'r') as f:
+        jsondata = json.loads(f.read())
 
     if 'q' not in request.args: # we return all data if no param
-        return make_response(jsonify(data), 200)
+        return make_response(jsonify(jsondata), 200)
 
     choice_key = keys[choice] # getting the name of the attribute to query by in the json
     key = 0 # the key of the data found
     max = -1 # max similarity between query and each json item
 
     # finding the json object that most closely matches the query param
-    for i in range(len(data)):
-        if similar(data[i][choice_key], request.args['q']) > max:
-            max = similar(data[i][choice_key], request.args['q'])
+    for i in range(len(jsondata)):
+        if similar(jsondata[i][choice_key], request.args['q']) > max:
+            max = similar(jsondata[i][choice_key], request.args['q'])
             key = i
 
-    return make_response(jsonify(data[key]), 200) # returning json of the found object
+    return make_response(jsonify(jsondata[key]), 200) # returning json of the found object
 
-# this is what allows you to run the app
-if __name__ == "__main__":
-    app.run(debug=True)
+
+# running site
+if __name__=='__main__':
+    # run this command with any additional arg to run in production
+    if len(sys.argv) > 1:
+        print('<< PROD >>')
+        os.system(f"gunicorn -b '127.0.0.1:{data['port']}' website:app")
+    # or just run without an additional arg to run in debug
+    else:
+        print('<< DEBUG >>')
+        app.run(debug=True)
